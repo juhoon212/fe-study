52ae26acd839b09160dd77ff8294dc66ba03a40e
juhoon212 <ws5501@naver.com>
2024-03-20 22:54:46 +0900

    callback

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 function App() {
+
+    const handleSelect = (selectedButton) => {
+        console.log(selectedButton)
+    }
+
     return (
         <div>
             <Header/>
@@ -20,7 +25,10 @@ function App() {
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton>Components</TabButton>
+                        <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+                        <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                        <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+                        <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                     </menu>
                 </section>
             </main>
